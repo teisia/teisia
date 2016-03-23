607a795e96ed08ecdc5679134102abe3444a67aa
Teisia Park <teisia@gmail.com>
2016-03-23 17:15:23 -0600

    almost done with projects page

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,11 +1,13 @@
+//nav
 $(document).ready(function(){
-    $(".button a").click(function(){
-        $(".overlay").fadeToggle(200);
-       $(this).toggleClass('btn-open').toggleClass('btn-close');
-    });
+  $(".button a").click(function(){
+      $(".overlay").fadeToggle(200);
+        $(this).toggleClass('btn-open').toggleClass('btn-close');
+  });
 });
+
 $('.overlay').on('click', function(){
-    $(".overlay").fadeToggle(200);
+  $(".overlay").fadeToggle(200);
     $(".button a").toggleClass('btn-open').toggleClass('btn-close');
-    open = false;
+      open = false;
 });
---
+++ readme.md
@@ -0,0 +1,1 @@
+portfolio site: teisia.co
