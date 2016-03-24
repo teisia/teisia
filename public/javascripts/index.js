5ddf6beb4b7d51f5221dd83fc049952191c70af0
Teisia Park <teisia@gmail.com>
2016-03-24 11:26:08 -0600

    added a lil easter egg

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,5 +1,6 @@
 //nav
 $(document).ready(function(){
+  console.log("i like the way you think! there's an easter egg on the homepage. here's a hint: snoop dogg said this in one of his songs, 'will i be the _ that i was?'");
   $(".button a").click(function(){
       $(".overlay").fadeToggle(200);
         $(this).toggleClass("btn-open").toggleClass("btn-close");
